7969e8352daae3970fb57701bbab45863bd27efc
chrispirina <33334781+chrispirina@users.noreply.github.com>
2018-10-17 14:30:35 +1100

    GameManager In Class Work

---
+++ Mud Dog/Assets/Scripts/DebugScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            GameManager.instance.GameOver();
+        }
+		
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            GameManager.instance.IncreaseMud(10);
+        }
+	}
+}
---
+++ Mud Dog/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { TITLE, INGAME, WIN, LOSE}
+public class GameManager : Singleton<GameManager> {
+    public int mudLevel = 0;
+    public GameState gameState;
+	
+    
+    public void GameOver()
+    {
+        Debug.Log("Game Over");
+    }
+
+    public void IncreaseMud(int newMud)
+    {
+        mudLevel += newMud;
+        Debug.Log("Mud Level: " +mudLevel.ToString());
+        if (mudLevel >= 100)
+            GameOver();
+    }
+}
